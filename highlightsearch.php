c62d7b37aafe0e84dd683e1a6bf3caebd33ff897
Imran Qasim <imran_qasim@hotmail.com>
2016-09-03 17:29:58 +0500

    functionality updated plugin changed from markjs to another

--- highlightsearch.php
+++ highlightsearch.php
@@ -9,18 +9,18 @@
 	*/
 	function xss__highlight_form(){
 		?>
-		<div class="container">
+		<div class="container fixed-top">
 		  <div class="panel panel-default">
 		    <div class="panel-heading">
-		      <form class="row searchTermForm" >
+		      <form class="row searchTermForm" method="post" onsubmit="return searchPrompt(document.getElementById('keyword').value); return false;" >
 		        <div class="col-xs-12">
 		          <div class="form-group">
 		            <label for="keyword">Search term:</label>
-		            <input type="text" class="form-control input-sm" placeholder="Type your term" value="" name="keyword" id="keyword">
+		            <input type="text"  class="form-control input-sm" placeholder="Type your term" value="" name="keyword" id="keyword">
 		          </div>
-		        <button type="button" class="btn btn-default btn-sm" name="perform">Perform</button>
-		        <a id="btn-next" class="btn btn-default btn-sm" name="next">Next</a>
-		        <a id="btn-prev" class="btn btn-default btn-sm" name="prev">Previous</a>
+		        <button type="submit" class="btn btn-default btn-sm" name="perform">Perform</button>
+		     <!--    <a id="btn-next" class="btn btn-default btn-sm" name="next">Next</a>
+		        <a id="btn-prev" class="btn btn-default btn-sm" name="prev">Previous</a> -->
 		        </div>
 
 		      </form>
@@ -31,8 +31,9 @@
 		<?php
 	}
 	function xs__enqueue_scripts(){
-		wp_enqueue_script( 'mark-js-script','https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.0.1/jquery.mark.es6.js', array('jquery'), '1.8', true );
-		wp_enqueue_script( 'mark-js-script-custom', plugins_url().'/highlightsearch/js/custom_script.js', array('mark-js-script'), '1.0', true);
+		//wp_enqueue_script( 'mark-js-script','https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.0.1/jquery.mark.es6.js', array('jquery'), '1.8', true );
+		wp_enqueue_script( 'highlight-js',plugins_url().'/highlightsearch/js/highlight.js', array('jquery'), '1.8', true );
+		wp_enqueue_script( 'highlight-custom', plugins_url().'/highlightsearch/js/custom_script.js', array('highlight-js'), '1.0', true);
 	}
 	add_action('wp_enqueue_scripts','xs__enqueue_scripts');
 	add_shortcode( 'search_term', 'xss__highlight_form' );
--- js/custom_script.js
+++ js/custom_script.js
@@ -1,57 +1,163 @@
+function clearHighlights() {
+  $('main').removeHighlight();
+  // $('.productsearchstringnotification,.restaurantsearchstringnotification').fadeOut();
+  highlightcounter = 0;
+}
 
-jQuery(function($) {
+var searchterm = false;
+var oldsearchterm = false;
+var counter = 1;
+function searchPrompt(searchterm) {
 
-  var $terms=[];
-  var $count=0;
-  var $i= 0;
-  var $context = $("main");
+  /* Make sure that we are on the menucard tab */
+  if (window.modrewriteprefix == undefined) {
+    window.modrewriteprefix = '/';
+  }
+
+  var categoryitem = $(this);
+  console.log(searchterm);
+  /* if the infotabs are present AND we're not on the menucard tab already, do that first */
+  if ($('.restaurantinfotab_selected').length && $('.restaurantinfotab_selected a').attr('id').substr(8).toLowerCase() != 'menucard') {
+    /* force MenuCard tab */
+    clickedTab = $('#infotab_MenuCard').parent();
+    clickedTabName = 'MenuCard';
+    /* load the contents of the MenuCard tab and update browser state when ready */
+    $('.tab_'+clickedTabName.toLowerCase()).load(window.modrewriteprefix+'show'+ucfirst(clickedTabName)+'.php', function() {
+      /* push new browser state using History.js methods */
+      History.pushState({clickedTabName:clickedTabName,category:categoryitem.attr('id')}, clickedTab.attr('title'), clickedTab.children('a').first().attr('href'));
+      searchPrompt(searchterm); // start the search
+    });
+  }
+  else
+  {
+    if (oldsearchterm != searchterm)  {
+      counter = 1;
+      oldsearchterm = searchterm;
+
+      clearHighlights();
+      $('main').highlight(searchterm);
+      $('main').highlight(searchterm);
+
+      showSearchNotification(counter,highlightcounter)
+      //$('.searchPanel .productsearchstringnotification').fadeIn().html(counter + ' / ' + highlightcounter)
+    }
+    else
+    {
+      counter++;
+      if (counter > highlightcounter) counter = 1;
+      showSearchNotification(counter,highlightcounter)
+      //$('.searchPanel .productsearchstringnotification').fadeIn().html(counter + ' / ' + highlightcounter)
+    }
+
+    scrollToFirstHit(counter);
+  }
+
+  return false;
+}
+function showSearchNotification(counter,highlightcounter) {
+  if (highlightcounter==0) {
+    $('.searchPanel .productsearchstringcounter').hide()
+    $('.productsearchstringnotification, .restaurantsearchstringnotification').fadeIn()
+    return false;
+  }
+  $('.searchPanel .productsearchstringcounter').fadeIn().html(counter + ' / ' + highlightcounter)
+}
+
+
+function scrollToFirstHit(counter) {
+
+  if (highlightcounter==0) {
+    $('.productsearchstringnotification, .restaurantsearchstringnotification').fadeIn()
+  }
+  // mobile search notification
+  if (highlightcounter > 0) {
+
+    var prev = (counter-1)
+    var next = (counter+1)
+
+    if (prev < 1 ) prev = highlightcounter
+    if (next > highlightcounter ) next = 1
+
+
+    $('.searchnavigation .controls').fadeIn();
+
+    var click = '<a href="#" onclick="scrollToFirstHit('+prev+'); return false;"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-backward fa-stack-1x fa-inverse"></i></span></a>';
+    click += '<span class="counter">'+counter + ' / ' + highlightcounter+'</span>';
+    click += '<a href="#" onclick="scrollToFirstHit('+next+'); return false;"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-forward fa-stack-1x fa-inverse"></i></span></a>'
+
+    //var clicktag = "<a href=\"#\" onclick=\"scrollToFirstHit(); return false;\"><i class=\"fa fa-backward\"></i> <span class=\"counter\"></span> <i class=\"fa fa-forward\"></i></a>";
+    $('.searchnavigation .controls').fadeIn().html(click)
+  } else {
+    $('.searchnavigation .controls').fadeOut();
+  }
+
+  /* scroll to first found occurence */
+  i = 1;
+
+  $('.highlight').each(function() {
+
+    if (i==counter)
+    {
+      scrollToObject = false;
+      if (!$(this).is(':visible')) {
+        if ($(this).parent().is(':selected')) {
+          scrollToObject = $(this).parents('select');
+        }
+      } else {
+        scrollToObject = $(this);
+      }
+      //i++;
+
+      if (scrollToObject) {
+        /* Scroll speed is based on absolute distance. And previous started scroll animation are stopped in their track */
+        scrollDistance = ($(window).scrollTop() - (scrollToObject.offset().top - $('#splash').height() - 15));
+        if (scrollDistance < 0) scrollDistance = scrollDistance * -1;
+        /* Don't use variable speed on distances exceeding 2000 pixels */
+        scrollSpeed = scrollDistance < 2000 ? scrollDistance : 2000;
+        $('html,body').stop( true, true ).animate(
+          {scrollTop: scrollToObject.offset().top - $('#splash').height() - 15},
+          scrollSpeed
+        );
+
+        return false;
+      }
+
+    }
+    i++
+
+  });
+
+  return false;
+}
+
+jQuery(document).ready(function($) {
+  /* make sure both search fields have the same content */
+  $('.step3 #iproductsearchstring, #irestaurantsearchstring').keyup(function(e) {
+    $('.step3 #iproductsearchstring2, #irestaurantsearchstring2').val($(this).val());
+    /* if empty, clear all highlights */
+    if ($(this).val() == '') {
+      clearHighlights();
+      $('.searchnavigation .controls').fadeOut();
+    }
+  });
+
+  $('.step3 #iproductsearchstring2, #irestaurantsearchstring2').keyup(function(e) {
+    $('.step3 #iproductsearchstring, #irestaurantsearchstring').val($(this).val());
+    /* if empty, clear all highlights */
+    if ($(this).val() == '') {
+      clearHighlights();
+      $('.searchnavigation .controls').fadeOut();
+    }
+  });
+
+});
+
+jQuery(function($){
   var $form = $(".searchTermForm");
   var $button = $form.find("button[name='perform']");
   var $input = $form.find("input[name='keyword']");
-  $button.on("click.perform", function() {
-    var searchTerm = $input.val();
-    var options = {};
-    var values = $form.serializeArray();
-   
-    $context.unmark();
-
-    var options = {
-    "filter": function(node, term, totalCounter, counter){
-          $count=totalCounter;
-          $('a#btn-next').attr('href','javascript:;');
-          $('a#btn-prev').attr('href','javascript:;');
-          return true;
-    }
-};
-    options["className"] ="search-found";
-    
-    $context.mark(searchTerm, options);
-
+  $button.on("click", function() {
+      $('main').removeHighlight().highlight($input.val());
   });
-  //$button.trigger("click.perform");
-
-  $('a#btn-next').on('click',function(){ 
-    
-   if($i<0){
-    $i = 0;
-   }
-    if($i <= parseInt($count)){
-      $('mark').eq($i).attr('id', 'term-'+$i);
-      $('mark').eq($i).css({"background":"yellow"});
-      $i++;
-    } 
-    console.log($i);
-  });
-    $('a#btn-prev').on('click',function(){
-    
-     if ($i > parseInt($count)){
-      $i = $count;
-     }
-      if($i>=0){ 
-           $('mark').eq($i).attr('id', 'term-'+$i);
-          $('mark').eq($i).css({"background":"orange"});
-          $i--;
-      }
-        console.log($i);
-  });
+  
 });
