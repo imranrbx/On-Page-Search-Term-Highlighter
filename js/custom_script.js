c1458d885765e38b94b5c02d79d72199a4fc657d
Imran Qasim <imran_qasim@hotmail.com>
2016-09-03 16:20:36 +0500

    Improvement in script

--- js/custom_script.js
+++ js/custom_script.js
@@ -3,7 +3,7 @@ jQuery(function($) {
 
   var $terms=[];
   var $count=0;
-  //var $i=1;
+  var $i= 0;
   var $context = $("main");
   var $form = $(".searchTermForm");
   var $button = $form.find("button[name='perform']");
@@ -30,22 +30,28 @@ jQuery(function($) {
   });
   //$button.trigger("click.perform");
 
-  $('a#btn-next').on('click',$i=0,function(){ 
-    if($i <= $count){
+  $('a#btn-next').on('click',function(){ 
+    
+   if($i<0){
+    $i = 0;
+   }
+    if($i <= parseInt($count)){
       $('mark').eq($i).attr('id', 'term-'+$i);
       $('mark').eq($i).css({"background":"yellow"});
-      console.log($i);
       $i++;
     } 
+    console.log($i);
   });
-    $('a#btn-prev').on('click',$i=$count,function(){
+    $('a#btn-prev').on('click',function(){
+    
+     if ($i > parseInt($count)){
+      $i = $count;
+     }
       if($i>=0){ 
            $('mark').eq($i).attr('id', 'term-'+$i);
           $('mark').eq($i).css({"background":"orange"});
-          console.log($i);
           $i--;
       }
-   
-      
+        console.log($i);
   });
 });
